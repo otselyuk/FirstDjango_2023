d0eba73b9d16bcc4260f2bf63c7160f9dc82b610
otselyuk <otselyuk@inbox.ru>
2023-07-14 13:05:50 +0300

    static add

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from MainApp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -8,4 +10,4 @@ urlpatterns = [
     path('about', views.about, name='about'),
     path('item/<int:id>', views.get_item, name='item-detail'),
     path('items', views.items_list, name='items-list')
-]
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
